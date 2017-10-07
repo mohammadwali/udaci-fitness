eaec7b1bef42afbe3729c01a4fdcca9173333e00
Mohammad Wali <mohdwali@live.com>
2017-10-07 16:20:15 +0530

    Adding Submit button to addEntry page

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import React, {Component} from 'react'
+import {View, TouchableOpacity, Text} from 'react-native'
+import {getMetricMetaInfo, timeToString} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+
+function SubmitBtn({onPress}) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     state = {
@@ -14,7 +23,7 @@ export default class AddEntry extends Component {
         eat: 0,
     }
     increment = (metric) => {
-        const { max, step } = getMetricMetaInfo(metric)
+        const {max, step} = getMetricMetaInfo(metric)
 
         this.setState((state) => {
             const count = state[metric] + step
@@ -40,6 +49,13 @@ export default class AddEntry extends Component {
             [metric]: value
         }))
     }
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        this.setState(() => ({run: 0, bike: 0, swim: 0, sleep: 0, eat: 0}))
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo()
 
@@ -47,7 +63,7 @@ export default class AddEntry extends Component {
             <View>
                 <DateHeader date={(new Date()).toLocaleDateString()}/>
                 {Object.keys(metaInfo).map((key) => {
-                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const {getIcon, type, ...rest} = metaInfo[key]
                     const value = this.state[key]
 
                     return (
@@ -68,6 +84,7 @@ export default class AddEntry extends Component {
                         </View>
                     )
                 })}
+                <SubmitBtn onPress={this.submit}/>
             </View>
         )
     }
