363d0a8c8d1d04528fd560b3e73c97c1fba6d8e9
Mohammad Wali <mohdwali@live.com>
2017-10-08 18:30:28 +0530

    Returning to home after adding entry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import {View, TouchableOpacity, Text, StyleSheet, Platform} from 'react-native'
-import {getMetricMetaInfo, timeToString, getDailyReminderValue} from '../utils/helpers'
+import {
+    getMetricMetaInfo,
+    timeToString,
+    getDailyReminderValue
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
@@ -10,6 +14,7 @@ import {submitEntry, removeEntry} from '../utils/api'
 import {connect} from 'react-redux'
 import {addEntry} from '../actions'
 import {purple, white} from '../utils/colors'
+import {NavigationActions} from 'react-navigation'
 
 function SubmitBtn({onPress}) {
     return (
@@ -65,7 +70,7 @@ class AddEntry extends Component {
 
         this.setState(() => ({run: 0, bike: 0, swim: 0, sleep: 0, eat: 0}))
 
-        // Navigate to home
+        this.toHome()
 
         submitEntry({key, entry})
 
@@ -78,9 +83,12 @@ class AddEntry extends Component {
             [key]: getDailyReminderValue()
         }))
 
-        // Route to Home
+        this.toHome()
 
         removeEntry(key)
+    }
+    toHome = () => {
+        this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
     }
 
     render() {
