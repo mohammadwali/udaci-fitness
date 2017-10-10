b74ecb8be7c1fe0142849441dd986b778ae6fb09
Mohammad Wali <mohdwali@live.com>
2017-10-10 14:21:35 +0530

    Adding animation to direction text

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet} from 'react-native'
+import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated} from 'react-native'
 import {Foundation} from '@expo/vector-icons'
 import {purple, white} from '../utils/colors'
 import {Location, Permissions} from 'expo'
@@ -9,33 +9,32 @@ export default class Live extends Component {
     state = {
         coords: null,
         status: null,
-        direction: ''
+        direction: '',
+        bounceValue: new Animated.Value(1),
     }
-
-    componentDidMount() {
+    componentDidMount () {
         Permissions.getAsync(Permissions.LOCATION)
-            .then(({status}) => {
+            .then(({ status }) => {
                 if (status === 'granted') {
                     return this.setLocation()
                 }
 
-                this.setState(() => ({status}))
+                this.setState(() => ({ status }))
             })
             .catch((error) => {
                 console.warn('Error getting Location permission: ', error)
 
-                this.setState(() => ({status: 'undetermined'}))
+                this.setState(() => ({ status: 'undetermined' }))
             })
     }
-
     askPermission = () => {
         Permissions.askAsync(Permissions.LOCATION)
-            .then(({status}) => {
+            .then(({ status }) => {
                 if (status === 'granted') {
                     return this.setLocation()
                 }
 
-                this.setState(() => ({status}))
+                this.setState(() => ({ status }))
             })
             .catch((error) => console.warn('error asking Location permission: ', error))
     }
@@ -44,8 +43,16 @@ export default class Live extends Component {
             enableHighAccuracy: true,
             timeInterval: 1,
             distanceInterval: 1,
-        }, ({coords}) => {
+        }, ({ coords }) => {
             const newDirection = calculateDirection(coords.heading)
+            const { direction, bounceValue } = this.state
+
+            if (newDirection !== direction) {
+                Animated.sequence([
+                    Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+                    Animated.spring(bounceValue, { toValue: 1, friction: 4})
+                ]).start()
+            }
 
             this.setState(() => ({
                 coords,
@@ -54,9 +61,8 @@ export default class Live extends Component {
             }))
         })
     }
-
     render() {
-        const {status, coords, direction} = this.state
+        const { status, coords, direction, bounceValue } = this.state
 
         if (status === null) {
             return <ActivityIndicator style={{marginTop: 30}}/>
@@ -65,10 +71,9 @@ export default class Live extends Component {
         if (status === 'denied') {
             return (
                 <View style={styles.center}>
-                    <Foundation name='alert' size={50}/>
+                    <Foundation name='alert' size={50} />
                     <Text>
-                        You denied your location. You can fix this by visiting your settings and enabling location
-                        services for this app.
+                        You denied your location. You can fix this by visiting your settings and enabling location services for this app.
                     </Text>
                 </View>
             )
@@ -77,7 +82,7 @@ export default class Live extends Component {
         if (status === 'undetermined') {
             return (
                 <View style={styles.center}>
-                    <Foundation name='alert' size={50}/>
+                    <Foundation name='alert' size={50} />
                     <Text>
                         You need to enable location services for this app.
                     </Text>
@@ -94,9 +99,10 @@ export default class Live extends Component {
             <View style={styles.container}>
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
-                    <Text style={styles.direction}>
+                    <Animated.Text
+                        style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
                         {direction}
-                    </Text>
+                    </Animated.Text>
                 </View>
                 <View style={styles.metricContainer}>
                     <View style={styles.metric}>
@@ -140,7 +146,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         margin: 20,
     },
-    buttonText: {
+    buttonText :{
         color: white,
         fontSize: 20,
     },
